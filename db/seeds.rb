a865026e48edeca2931cc9f82f93ea2a3c618802
David P. Steelman <dsteelma@umd.edu>
2016-03-31 13:58:07 -0400

    LIBITD-389. Added departments to seeds.rb
    https://issues.umd.edu/browse/LIBITD-389

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,33 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-division_names = %w(ASD CSS DO DSS PSD)
+division_names = [
+  'Administrative Services', 'Collections Strategies & Services',
+  "Dean's Office", 'Digital Systems & Stewardship', 'Public Services' ]
+
 division_names.each { |name| Division.create!(name: name) }
+
+departments_by_division = {
+  'Administrative Services' => [ 'Budget & Business Services',
+                                 'Libraries Facilities',
+                                 'Libraries Human Resources' ],
+  'Collections Strategies & Services' => [ 'Acquisitions',
+                                           'Collections Development',
+                                           'Metadata Services',
+                                           'Prange',
+                                           'Special Collections & University Archives' ],
+  "Dean's Office" => [ 'Communications', "Dean's Office" ],
+  'Digital Systems & Stewardship' => [ 'Digitization',
+                                       'Digital Data Services',
+                                       'Digital Preservation Initiatives',
+                                       'Software Support',
+                                       'User Systems & Support' ],
+  'Public Services' => [ 'Public Services', 'Research & Learning' ] }
+
+departments_by_division.each do |division_name, departments|
+  division = Division.find_by_name(division_name)
+  departments.each { |dept_name| division.departments.create!(name: dept_name) }
+end
+
+
+

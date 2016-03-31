4d1b80eba8f70fe54aa0860fe9106d68210db768
David P. Steelman <dsteelma@umd.edu>
2016-03-31 13:06:09 -0400

    LIBITD-389. Added division names to seeds.rb
    https://issues.umd.edu/browse/LIBITD-389

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+division_names = %w(ASD CSS DO DSS PSD)
+division_names.each { |name| Division.create!(name: name) }

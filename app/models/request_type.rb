677d980e379e0ff92ba4580716c3d7a6a2f617b6
David P. Steelman <dsteelma@umd.edu>
2016-04-01 11:18:24 -0400

    LIBITD-389. Added tests for RequestType model.
    https://issues.umd.edu/browse/LIBITD-389

--- app/models/request_type.rb
+++ app/models/request_type.rb
@@ -1,2 +1,5 @@
+# A type of personnel request (New, Renewal, Pay Adjustment, etc.)
 class RequestType < ActiveRecord::Base
+  validates :code, presence: true
+  validates :name, presence: true
 end
--- test/models/request_type_test.rb
+++ test/models/request_type_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
+# Tests for the "RequestType" model
 class RequestTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @request_type = request_types(:one)
+  end
+
+  test 'should be valid' do
+    assert @request_type.valid?
+  end
+
+  test 'code should be present' do
+    @request_type.code = '  '
+    assert_not @request_type.valid?
+  end
+
+  test 'name should be present' do
+    @request_type.name = '  '
+    assert_not @request_type.valid?
+  end
 end

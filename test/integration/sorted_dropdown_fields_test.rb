7d079ade46cc865c7cba97d472978c537b5ac936
David P. Steelman <dsteelma@umd.edu>
2016-04-20 15:30:28 -0400

    LIBITD-413. Modified dropdowns to display entries in alphabetical order
    Added test/shared/ directory to contain modules that can be
    resused in various tests.
    
    Updated test/test_helper.rb to automatically "require" files in
    test/shared/ directory.
    
    This use of modules was suggested by the following page:
    
    http://schock.net/articles/2015/01/21/modules-with-rails-tests-share-behavior-minitest/
    
    https://issues.umd.edu/browse/LIBITD-413

---
+++ test/integration/sorted_dropdown_fields_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+
+# Verifies sorted dropdowns in ContractorRequest "new" page
+class SortedDropdownFieldsContractorRequestNewTest < ActionDispatch::IntegrationTest
+  def setup
+    get new_contractor_request_path
+    @field_prefix = 'contractor_request'
+    @valid_category_code = ContractorRequest::VALID_EMPLOYEE_CATEGORY_CODE
+    @valid_request_type_codes = ContractorRequest::VALID_REQUEST_TYPE_CODES
+  end
+
+  include DepartmentDropdownTest
+  include SubdepartmentDropdownTest
+  include EmployeeTypeDropdownTest
+  include RequestTypeDropdownTest
+end
+
+# Verifies sorted dropdowns in LaborRequest "new" page
+class SortedDropdownFieldsLaborRequestNewTest < ActionDispatch::IntegrationTest
+  def setup
+    get new_labor_request_path
+    @field_prefix = 'labor_request'
+    @valid_category_code = LaborRequest::VALID_EMPLOYEE_CATEGORY_CODE
+    @valid_request_type_codes = LaborRequest::VALID_REQUEST_TYPE_CODES
+  end
+
+  include DepartmentDropdownTest
+  include SubdepartmentDropdownTest
+  include EmployeeTypeDropdownTest
+  include RequestTypeDropdownTest
+end
+
+# Verifies sorted dropdowns in StaffRequest "new" page
+class SortedDropdownFieldsStaffRequestNewTest < ActionDispatch::IntegrationTest
+  def setup
+    get new_staff_request_path
+    @field_prefix = 'staff_request'
+    @valid_category_code = StaffRequest::VALID_EMPLOYEE_CATEGORY_CODE
+    @valid_request_type_codes = StaffRequest::VALID_REQUEST_TYPE_CODES
+  end
+
+  include DepartmentDropdownTest
+  include SubdepartmentDropdownTest
+  include EmployeeTypeDropdownTest
+  include RequestTypeDropdownTest
+end
+
+# Verifies sorted dropdowns in Department "new" page
+class SortedDropdownFieldsDepartmentNewTest < ActionDispatch::IntegrationTest
+  def setup
+    get new_department_path
+    @field_prefix = 'department'
+  end
+
+  include DivisionDropdownTest
+end
+
+# Verifies sorted dropdowns in Subdepartment "new" page
+class SortedDropdownFieldsSubdepartmentNewTest < ActionDispatch::IntegrationTest
+  def setup
+    get new_subdepartment_path
+    @field_prefix = 'subdepartment'
+  end
+
+  include DepartmentDropdownTest
+end
+
+# Verifies sorted dropdowns in Employee Type "new" page
+class SortedDropdownFieldsEmployeeTypeNewTest < ActionDispatch::IntegrationTest
+  def setup
+    get new_employee_type_path
+    @field_prefix = 'employee_type'
+  end
+
+  include EmployeeCategoryDropdownTest
+end
---
+++ test/shared/sorted_dropdowns.rb
@@ -0,0 +1,119 @@
+# As suggested in http://schock.net/articles/2015/01/21/modules-with-rails-tests-share-behavior-minitest/
+
+# Test for sorted "Department" dropdown entries
+module DepartmentDropdownTest
+  extend ActiveSupport::Concern
+
+  included do
+    test 'department dropdown field should be sorted' do
+      sorted_department_names = Department.order('name').collect(&:name)
+
+      # Prepend dropdown prompt
+      sorted_department_names.unshift('Select Department')
+
+      assert_select 'select[id=?] option', "#{@field_prefix}_department_id" do |opts|
+        opts.each do |opt|
+          assert_equal sorted_department_names.shift, opt.text
+        end
+      end
+    end
+  end
+end
+
+# Test for sorted "Subdepartment" dropdown entries
+module SubdepartmentDropdownTest
+  extend ActiveSupport::Concern
+  included do
+    test 'subdepartment dropdown field should be sorted' do
+      sorted_subdepartment_names = Subdepartment.order('name').collect(&:name)
+
+      # Prepend dropdown prompt
+      sorted_subdepartment_names.unshift('Select Subdepartment')
+
+      assert_select 'select[id=?] option', "#{@field_prefix}_subdepartment_id" do |opts|
+        opts.each do |opt|
+          assert_equal sorted_subdepartment_names.shift, opt.text
+        end
+      end
+    end
+  end
+end
+
+# Test for sorted "Employee Category" dropdown entries
+module EmployeeCategoryDropdownTest
+  extend ActiveSupport::Concern
+  included do
+    test 'employee category dropdown field should be sorted' do
+      sorted_employee_category_names = EmployeeCategory.order('name').collect(&:name)
+
+      # Prepend dropdown prompt
+      sorted_employee_category_names.unshift('Select Category')
+
+      assert_select 'select[id=?] option', "#{@field_prefix}_employee_category_id" do |opts|
+        opts.each do |opt|
+          assert_equal sorted_employee_category_names.shift, opt.text
+        end
+      end
+    end
+  end
+end
+
+# Test for sorted "Employee Type" dropdown entries
+module EmployeeTypeDropdownTest
+  extend ActiveSupport::Concern
+  included do
+    test 'employee type dropdown field should be sorted' do
+      sorted_employee_type_names = EmployeeType.employee_types_with_category(@valid_category_code)
+                                               .order('name').collect(&:name)
+
+      # Prepend dropdown prompt
+      sorted_employee_type_names.unshift('Select Employee Type')
+
+      assert_select 'select[id=?] option', "#{@field_prefix}_employee_type_id" do |opts|
+        opts.each do |opt|
+          assert_equal sorted_employee_type_names.shift, opt.text
+        end
+      end
+    end
+  end
+end
+
+# Test for sorted "Request Type" dropdown entries
+module RequestTypeDropdownTest
+  extend ActiveSupport::Concern
+  included do
+    test 'request type dropdown field should be sorted' do
+      sorted_request_type_names = RequestType.order('name')
+                                             .select { |a| @valid_request_type_codes.include?(a.code) }
+                                             .collect(&:name)
+
+      # Prepend dropdown prompt
+      sorted_request_type_names.unshift('Select Request Type')
+
+      assert_select 'select[id=?] option', "#{@field_prefix}_request_type_id" do |opts|
+        opts.each do |opt|
+          assert_equal sorted_request_type_names.shift, opt.text
+        end
+      end
+    end
+  end
+end
+
+# Test for sorted "Division" dropdown entries
+module DivisionDropdownTest
+  extend ActiveSupport::Concern
+  included do
+    test 'division dropdown field should be sorted' do
+      sorted_request_type_names = Division.order('name').collect(&:name)
+
+      # Prepend dropdown prompt
+      sorted_request_type_names.unshift('Select Division')
+
+      assert_select 'select[id=?] option', "#{@field_prefix}_division_id" do |opts|
+        opts.each do |opt|
+          assert_equal sorted_request_type_names.shift, opt.text
+        end
+      end
+    end
+  end
+end

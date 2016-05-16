275cb9cdd453c140b31c43388dacbf7440288bc8
David P. Steelman <dsteelma@umd.edu>
2016-05-16 11:55:44 -0400

    LIBITD-431. Database migration and Rubocop fixed.
    https://issues.umd.edu/browse/LIBITD-431

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428175631) do
+ActiveRecord::Schema.define(version: 20160516155002) do
 
   create_table "contractor_requests", force: :cascade do |t|
     t.integer  "employee_type_id"
@@ -126,5 +126,12 @@ ActiveRecord::Schema.define(version: 20160428175631) do
   end
 
   add_index "units", ["department_id"], name: "index_units_on_department_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "cas_directory_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end

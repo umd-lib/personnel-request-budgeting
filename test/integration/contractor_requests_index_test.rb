80cd8e539c42ed54b474007bf9a1d13b88595b3c
David P. Steelman <dsteelma@umd.edu>
2016-05-23 11:35:38 -0400

    LIBITD-434. Added "Division" column to personnel request pages.
    https://issues.umd.edu/browse/LIBITD-434

--- test/integration/contractor_requests_index_test.rb
+++ test/integration/contractor_requests_index_test.rb
@@ -15,7 +15,7 @@ class ContractorRequestsIndexTest < ActionDispatch::IntegrationTest
   test 'index including pagination and sorting' do
     columns = %w(position_description employee_type_code request_type_code
                  contractor_name number_of_months annual_base_pay nonop_funds
-                 department_code unit_code)
+                 division_code department_code unit_code)
 
     get contractor_requests_path
     assert_template 'contractor_requests/index'
@@ -23,16 +23,45 @@ class ContractorRequestsIndexTest < ActionDispatch::IntegrationTest
     # Verify sort links
     assert_select 'a.sort_link', count: columns.size
 
-    columns.each do |column|
-      %w(asc desc).each do |order|
-        q_param = { s: column + ' ' + order }
+    columns.each do |sort_column|
+      %w(asc desc).each do |sort_direction|
+        q_param = { s: sort_column + ' ' + sort_direction }
         get contractor_requests_path, q: q_param
         assert_template 'contractor_requests/index'
         assert_select 'ul.pagination'
-        ContractorRequest.ransack(q_param).result.paginate(page: 1).each do |entry|
-          assert_select 'a[href=?]', contractor_request_path(entry)
+
+        # Verify sorting by getting the text of the page, then checking the
+        # position of each entry -- the position should increase on each entry.
+        page = response.body
+
+        last_result_index = 0
+        results = ContractorRequest.ransack(q_param).result
+        results = sort_and_paginate_results(results, sort_column, sort_direction)
+        results.each do |entry|
+          entry_path = contractor_request_path(entry)
+          entry_index = page.index(entry_path)
+          assert_not_nil entry_index,
+                         "'#{entry_path}' could not be found when sorting '#{sort_column} #{sort_direction}'"
+          assert last_result_index < entry_index, "Failed for '#{q_param[:s]}'"
+          assert_select 'tr td a[href=?]', entry_path
+          last_result_index = entry_index
         end
       end
     end
   end
+
+  private
+
+    def sort_and_paginate_results(results, sort_column, sort_direction)
+      if sort_column != 'division_code'
+        results.paginate(page: 1)
+      else
+        # Division sorting needs additional join to access division code
+        sort_order = "divisions.code #{sort_direction}"
+        table_name = results.table_name
+        results = results.joins("LEFT OUTER JOIN departments on departments.id = #{table_name}.department_id")
+        results = results.joins('LEFT OUTER JOIN divisions on departments.division_id = divisions.id')
+        results.order(sort_order).paginate(page: 1)
+      end
+    end
 end

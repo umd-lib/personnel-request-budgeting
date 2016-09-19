98af23c5d490377e05b30ef14d6971926d74c973
David P. Steelman <dsteelma@umd.edu>
2016-09-19 12:16:58 -0400

    LIBITD-442. Added "Annual Cost" field to "show" page.
    https://issues.umd.edu/browse/LIBITD-442

--- test/integration/labor_requests_show_test.rb
+++ test/integration/labor_requests_show_test.rb
@@ -6,10 +6,15 @@ class LaborRequestsShowTest < ActionDispatch::IntegrationTest
     @labor_request = labor_requests(:fac_hrly_renewal)
   end
 
+  test 'annual cost field should be displayed' do
+    get labor_request_path(@labor_request)
+    assert_select '[id=annual_cost]', 1
+  end
+
   test 'currency field values show with two decimal places' do
     get labor_request_path(@labor_request)
 
-    currency_fields = %w(hourly_rate nonop_funds)
+    currency_fields = %w(hourly_rate annual_cost nonop_funds)
     currency_fields.each do |field|
       assert_select "[id=#{field}]" do |e|
         verify_two_digit_currency_field(field, e.text)

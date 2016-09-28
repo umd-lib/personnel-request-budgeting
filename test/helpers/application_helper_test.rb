2c9c861779ae3dd010b5f5537a2da9f67cc04ebf
David P. Steelman <dsteelma@umd.edu>
2016-09-28 12:02:51 -0400

    LIBITD-481. Initial implementation of help_text_icon
    https://issues.umd.edu/browse/LIBITD-481

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -7,4 +7,20 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal '', suppress_status('Under Review')
     assert_equal 'Hot', suppress_status('Hot')
   end
+
+  test 'verify help-text-icon content when provided a valid translation key' do
+    existing_translation_key = 'help_text.position_description'
+    expected_help_test = raw t('help_text.position_description')
+
+    html_output = help_text_icon(existing_translation_key)
+    assert html_output.include?(expected_help_test)
+    assert html_output.include?('class="help-text-icon"')
+  end
+
+  test 'verify help-text-icon content when provided an invalid translation key' do
+    invalid_translation_key = 'this.key.should.not.exist'
+
+    html_output = help_text_icon(invalid_translation_key)
+    assert html_output.nil?
+  end
 end

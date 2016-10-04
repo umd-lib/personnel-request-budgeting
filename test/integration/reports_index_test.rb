450bf71ee67fe743517aaeaabbbd62c2d659677e
David P. Steelman <dsteelma@umd.edu>
2016-10-04 15:18:34 -0400

    LIBITD-520. Modified test so that it actually fails for bad sorting
    Verifies that displayed strings are sorted, instead of relying on
    a comparison with Ransack.
    
    Fields represented by an "enum" have special handling, because the
    database sorting treats them as a numeric sort.
    
    https://issues.umd.edu/browse/LIBITD-520

--- test/integration/reports_index_test.rb
+++ test/integration/reports_index_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 # Integration test for the Reports index page
 class ReportsIndexTest < ActionDispatch::IntegrationTest
   test 'index including sorting' do
-    columns = %w( name format creator created_at status)
+    columns = %w(name format creator_name created_at status)
+    enum_fields = %w(format status)
 
     get reports_path
     assert_template 'reports/index'
@@ -18,12 +19,50 @@ class ReportsIndexTest < ActionDispatch::IntegrationTest
         get reports_path, q: q_param
         assert_template 'reports/index'
 
-        Report.ransack(q_param).result.each do |entry|
-          assert_select 'a[href=?]', report_path(entry)
-        end
+        verify_table_sorting(response, column, order, enum_fields)
       end
     end
   end
+
+  # rubocop:disable Metrics/AbcSize
+
+  # Verifies the sorting of a particular column in a table by comparing the
+  # order of the displayed strings.
+  #
+  # This method assumes that each row of the table contains a "td" element
+  # with a "headers" attribute corresponding to the field, i.e.:
+  #
+  # <td headers="name">...</td>
+  #
+  # @param response the page response
+  # @param column the column attribute being sorted
+  # @param order 'asc' if the order is ascending, 'desc', if descending
+  # @param enum_fields an Array of fields held as a numeric enumeration in the
+  #   model
+  # @return [void]
+  def verify_table_sorting(response, column, order, enum_fields)
+    doc = Nokogiri::HTML(response.body)
+
+    # Get the text for the column from each record, using "header" to
+    # identify the field value.
+    records_text = doc.xpath("//td[@headers='#{column}']").map(&:child).map(&:text)
+
+    # Postgres and Ruby collate spaces differently, so convert spaces to
+    # underscores
+    records_text.each { |r| r.tr!(' ', '_') }
+
+    # Enum fields should be converted to their numeric representation
+    if enum_fields.include?(column)
+      records_text.map! { |r| Report.send(column.pluralize)[r] }
+    end
+
+    expected_text = order == 'asc' ? records_text.sort : records_text.sort.reverse
+
+    assert_equal expected_text, records_text,
+                 "Improper sorting column '#{column}', order '#{order}'"
+  end
+
+  # rubocop:enable Metrics/AbcSize
 
   test 'there should be a delete button but not an edit button' do
     get reports_path
@@ -36,14 +75,14 @@ class ReportsIndexTest < ActionDispatch::IntegrationTest
   end
 
   test 'there should  be a d/l button for status=completed and no d/l for all others' do
-    stuck = reports(:stuck)
-    awesome = reports(:awesome)
+    report_with_error = reports(:report_with_error)
+    report_completed = reports(:report_completed)
     get reports_path
-    assert_select 'tr[id=?]', "report_#{stuck.id}" do
+    assert_select 'tr[id=?]', "report_#{report_with_error.id}" do
       assert_select 'a[class~="download"]', false
     end
 
-    assert_select 'tr[id=?]', "report_#{awesome.id}" do
+    assert_select 'tr[id=?]', "report_#{report_completed.id}" do
       assert_select 'a[class~="download"]'
     end
   end

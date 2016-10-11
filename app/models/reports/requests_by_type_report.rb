6f27842466603f926268549a619086bd2546c453
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2016-10-11 20:17:33 +0200

    LIBITD-512 More changes to the comments
    This changes some more comments
    
    https://issues.umd.edu/browse/LIBITD-512

--- app/models/reports/requests_by_type_report.rb
+++ app/models/reports/requests_by_type_report.rb
@@ -1,6 +1,6 @@
 require 'reportable'
 
-# A basic report that just gets all teh request types
+# A basic report that just gets all the request types
 class RequestsByTypeReport
   include Reportable
 
@@ -19,9 +19,6 @@ class RequestsByTypeReport
     end
   end
 
-  # this is actually not great....but, maybe we can figure this out..
-  # currently, this loads all the requests into memory. idealls, we can
-  # have this lazy load during iteration...
   def query
     [StaffRequest.enum_for(:find_each), ContractorRequest.enum_for(:find_each), LaborRequest.enum_for(:find_each)]
   end

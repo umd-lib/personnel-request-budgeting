2963e9a83784db6803881974adc1c9739144a6f7
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2017-01-11 11:43:43 -0500

    LIBITD-509. Updated the report.
    See: https://issues.umd.edu/browse/LIBITD-509?focusedCommentId=237448&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-237448
    
    https://issues.umd.edu/browse/LIBITD-509

--- app/models/reports/requests_count_by_review_status_report.rb
+++ app/models/reports/requests_count_by_review_status_report.rb
@@ -1,6 +1,6 @@
 require 'reportable'
 # A summary report for the requests count by review status
-class RequestCountsByReviewStatus
+class RequestsCountByReviewStatus
   include Reportable
   class << self
     # @return [String] human-readable description of the report, displayed in
@@ -23,14 +23,42 @@ class RequestCountsByReviewStatus
 
   # @return [Object] the data used by the template
   def query # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    under_review = nil
+    approved = nil
+    not_approved = nil
+    contingent = nil
+
+    ReviewStatus.all.to_a.each do |request|
+      status_count = Hash.new(0)
+      status_count[:labor_count] = request.labor_requests_count
+      status_count[:staff_count] = request.staff_requests_count
+      status_count[:contractor_count] = request.contractor_requests_count
+      if request.name == 'Under Review'
+        under_review = status_count
+      elsif request.name == 'Approved'
+        approved = status_count
+      elsif request.name == 'Not Approved'
+        not_approved = status_count
+      elsif request.name == 'Contingent'
+        contingent = status_count
+      end
+    end
+
+    category_display_name = Hash.new(0)
+    category_display_name[:labor_count] = 'Labor and Assistance'
+    category_display_name[:staff_count] = 'Staff'
+    category_display_name[:contractor_count] = 'Salaried Contractor'
+
+    category_order = [:labor_count, :staff_count, :contractor_count]
 
     data = []
-    ReviewStatus.all.to_a.each do |request|
-      value = { 
-        status: request.name,
-        contractor_count: request.contractor_requests_count,
-        labor_count: request.labor_requests_count,
-        staff_count: request.staff_requests_count
+    category_order.each do |key|
+      value = {
+        category: category_display_name[key],
+        under_review: under_review[key],
+        approved: approved[key],
+        not_approved: not_approved[key],
+        contingent: contingent[key]
       }
       data << value
     end

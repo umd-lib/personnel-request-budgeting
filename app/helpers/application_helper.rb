63afba74a42ac41bde397e20c22c308dcf9f2e51
Mohamed Mohideen Abdul Rasheed <mohideen@umd.edu>
2017-01-12 10:53:06 -0500

    LIBITD-526. Modify fields in Excel Export.
    - Rearranged columns as per issue
    - Set column widths for large text fields
    - Added formatting for worksheet
    - Added define_styles method to application helper for workbook styling.
    
    https://issues.umd.edu/browse/LIBITD-LIBITD-526

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,4 +43,40 @@ module ApplicationHelper
       t('confirm_delete_prompt.with_description', description: description)
     end
   end
+
+  # Returns style definitions for axlsx workbook formatting.
+  #
+  # @param [xlsx_package.workbook] object for creating the styles
+  # @return [Hash] style definitions that can be used to format workbook
+  # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+  def define_styles(wb)
+    # Define style components
+    title_font_size = 16
+    header_font_size = 14
+    summary_description_font_size = 14
+    summary_result_font_size = 12
+    title_font = { sz: title_font_size, b: true }
+    header_font = { sz: header_font_size, b: true }
+    summary_description_font = { sz: summary_description_font_size, b: true, i: true }
+    summary_result_font = { sz: summary_result_font_size, b: true }
+    currency_format = { format_code: '$#,##0.00' }
+    top_border = { border: { style: :thick, color: '000000', name: :top, edges: [:top] } }
+    bottom_border = { border: { style: :thick, color: '000000', name: :bottom, edges: [:bottom] } }
+
+    # Style definitions
+    styles = {}
+    styles[:currency] = wb.styles.add_style(currency_format)
+    styles[:extra_title] = wb.styles.add_style(title_font)
+    styles[:title] = wb.styles.add_style title_font.merge(bg_color: 'FFFF00')
+    styles[:header] = wb.styles.add_style header_font.merge(bg_color: 'D9D9D9')
+    styles[:header_bottom_border] = wb.styles.add_style header_font.merge(bg_color: 'D9D9D9')
+                                    .merge(bottom_border)
+    styles[:summary_description] = wb.styles.add_style summary_description_font
+    styles[:summary_result] = wb.styles.add_style summary_result_font.merge(currency_format)
+                              .merge(bg_color: 'D9D9D9')
+    styles[:summary_result_top_border] = wb.styles.add_style summary_result_font.merge(currency_format)
+                                         .merge(bg_color: 'D9D9D9').merge(top_border)
+    styles
+  end
+  # rubocop:enable Metrics/AbcSize,Metrics/MethodLength
 end

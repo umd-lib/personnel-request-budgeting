2fa4d0c00837f1b1f20c35882c1405ca2648ef9a
David P. Steelman <dsteelma@umd.edu>
2017-01-17 16:30:10 -0500

    LIBITD-537. Rubocop fixes and test cleanup
    In labor_requests_cost_summary_report_test.rb changed class variables
    to instance variables, except for "@@spreadsheet" which is still a
    class variable so the "spreadsheet" method short-circuits, instead of
    running once for each test.
    
    https://issues.umd.edu/browse/LIBITD-537

--- test/integration/reports/labor_requests_cost_summary_report_test.rb
+++ test/integration/reports/labor_requests_cost_summary_report_test.rb
@@ -7,9 +7,10 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
 
   def before_all
     @@spreadsheet = nil
-    @@temp_file = nil
-    @@creation_date = nil
-    @@report_params = nil
+  end
+
+  def setup
+    spreadsheet unless @@spreadsheet
   end
 
   # @return [Roo::Spreadsheet] the spreadsheet to use for testing.
@@ -19,27 +20,27 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
     return @@spreadsheet if @@spreadsheet
 
     # Set review status ids to include in the report
-    review_status_ids = ReviewStatus.all.map { |rs| rs.id }
+    review_status_ids = ReviewStatus.all.map(&:id)
 
     report_user = users(:test_user)
-    @@report_params = { name: 'LaborRequestsCostSummaryReport',
+    @report_params = { name: 'LaborRequestsCostSummaryReport',
                       format: 'xlsx',
                       user_id: report_user.id,
                       parameters: { review_status_ids: review_status_ids }
                     }
-    report = Report.new(@@report_params)
+    report = Report.new(@report_params)
     ReportJob.perform_now report
     report_id = report.id
-    @@creation_date = report.created_at
+    @creation_date = report.created_at
 
     get report_download_url(id: report_id, format: 'xlsx')
     assert_response :success
 
-    @@temp_file = Tempfile.new(['test_temp', '.xlsx'], encoding: 'ascii-8bit')
+    @temp_file = Tempfile.new(['test_temp', '.xlsx'], encoding: 'ascii-8bit')
 
-    @@temp_file.write response.body
-    @@temp_file.close
-    @@spreadsheet = Roo::Excelx.new(@@temp_file.path)
+    @temp_file.write response.body
+    @temp_file.close
+    @@spreadsheet = Roo::Excelx.new(@temp_file.path)
   end
 
   test 'spreadsheet should have worksheet for each division' do
@@ -53,7 +54,7 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
   end
 
   test 'each worksheet should contain parameter information about the report' do
-    review_status_ids = @@report_params[:parameters][:review_status_ids]
+    review_status_ids = @report_params[:parameters][:review_status_ids]
     review_status_description = review_status_ids.map { |id| ReviewStatus.find(id).name }.join(', ')
 
     spreadsheet.each_with_pagename do |name, sheet|
@@ -66,13 +67,13 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
   test 'each worksheet should indicate the report creation date' do
     spreadsheet.each_with_pagename do |name, sheet|
       parsed = sheet.parse
-      assert(parsed.any? { |e| e.to_s.include?("#{@@creation_date}") },
+      assert(parsed.any? { |e| e.to_s.include?("#{@creation_date}") },
         "Could not find creation date on '#{name}' worksheet")
     end
   end
 
   # Runs after all tests, and ensures that the temp file is deleted.
   def after_all
-    @@temp_file.delete if @@temp_file
+    @temp_file.delete if @temp_file
   end
 end

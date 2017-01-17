e2b1ffad6f17149fd629f75845880d3d229d5e45
David P. Steelman <dsteelma@umd.edu>
2017-01-17 14:00:59 -0500

    LIBITD-537. Added integration test for report parameter and creation date
    Test that the creation date, and the review statuses used as selection
    parameters appear somewhere on every worksheet.
    
    https://issues.umd.edu/browse/LIBITD-537

--- test/integration/reports/labor_requests_cost_summary_report_test.rb
+++ test/integration/reports/labor_requests_cost_summary_report_test.rb
@@ -8,6 +8,8 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
   def before_all
     @@spreadsheet = nil
     @@temp_file = nil
+    @@creation_date = nil
+    @@report_params = nil
   end
 
   # @return [Roo::Spreadsheet] the spreadsheet to use for testing.
@@ -18,16 +20,17 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
 
     # Set review status ids to include in the report
     review_status_ids = ReviewStatus.all.map { |rs| rs.id }
-    
+
     report_user = users(:test_user)
-    report_params = { name: 'LaborRequestsCostSummaryReport',
+    @@report_params = { name: 'LaborRequestsCostSummaryReport',
                       format: 'xlsx',
                       user_id: report_user.id,
                       parameters: { review_status_ids: review_status_ids }
                     }
-    report = Report.new(report_params)
+    report = Report.new(@@report_params)
     ReportJob.perform_now report
     report_id = report.id
+    @@creation_date = report.created_at
 
     get report_download_url(id: report_id, format: 'xlsx')
     assert_response :success
@@ -49,9 +52,27 @@ class LaborRequestCostSummaryReportTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test 'each worksheet should contain parameter information about the report' do
+    review_status_ids = @@report_params[:parameters][:review_status_ids]
+    review_status_description = review_status_ids.map { |id| ReviewStatus.find(id).name }.join(', ')
+
+    spreadsheet.each_with_pagename do |name, sheet|
+      parsed = sheet.parse
+      assert(parsed.any? { |e| e.to_s.include?(review_status_description) },
+        "Could not find parameter information on '#{name}' worksheet")
+    end
+  end
+
+  test 'each worksheet should indicate the report creation date' do
+    spreadsheet.each_with_pagename do |name, sheet|
+      parsed = sheet.parse
+      assert(parsed.any? { |e| e.to_s.include?("#{@@creation_date}") },
+        "Could not find creation date on '#{name}' worksheet")
+    end
+  end
+
   # Runs after all tests, and ensures that the temp file is deleted.
   def after_all
     @@temp_file.delete if @@temp_file
   end
-
 end

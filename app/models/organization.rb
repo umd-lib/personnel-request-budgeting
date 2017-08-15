0f61df600207d47d2331b7b3258d36af3745912e
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2017-08-15 14:10:02 +0200

    LIBITD-925 Make more obvious what user and org permissions are
    This tries to make it more clean in UI what the current roles are in
    relation to orgs and users. User detail pages show what orgs are
    included in the user's roles ( all orgs via the hierachy ).
    
    The same is done for orgs, with org detail pages showing what users have
    access to the org going up the ancestor chain.
    
    The goal is to make it more clear what users have access to what orgs.
    
    Also adds some UI elements to the org page to show what the status and
    types are.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -10,7 +10,10 @@ class Organization < ApplicationRecord
     def root_org
       root.first
     end
+  
   end
+    
+  TYPE_MAPPING = { "root" => 'queen', 'division' => 'bishop', 'department' => 'knight', 'unit' => 'pawn' }.freeze 
 
   belongs_to :parent, class_name: 'Organization', foreign_key: 'organization_id'
   validates :organization_id, presence: true, unless: :root?
@@ -27,6 +30,7 @@ class Organization < ApplicationRecord
   has_many :roles, dependent: :delete_all
   validates_associated :roles
   accepts_nested_attributes_for :roles, reject_if: :all_blank, allow_destroy: true
+  has_many :users, through: :roles
 
   validates :code, :organization_type, :name, presence: true
 
@@ -47,8 +51,10 @@ class Organization < ApplicationRecord
     Time.zone.today > organization_cutoff.cutoff_date
   end
 
+ 
+  # the name of the record used in the json expression
   def text
-    "#{name} (#{code})"
+    "#{name} (#{code}) <i class='glyphicon glyphicon-#{ cutoff? ? 'ban-circle' : 'ok-circle' }'></i>"
   end
 
   def grandchildren
@@ -59,15 +65,30 @@ class Organization < ApplicationRecord
     grandchildren.map(&:children).flatten || []
   end
 
+  # returns an array of the organizations up the tree
+  def ancestors
+    mom = parent
+    ancestors = []
+    while mom
+      ancestors << mom
+      mom = mom.parent
+    end
+    ancestors
+  end
+
   def description
     "#{name} ( #{organization_type} )"
   end
 
+  def type_icon
+    "glyphicon glyphicon-#{TYPE_MAPPING[organization_type]}"
+  end
+  
   def as_json(options = {})
     org = super
     org[:id] = org['id']
     org[:parent] = org['organization_id'] || '#'
-    org[:icon] = 'glyphicon glyphicon-home'
+    org[:icon] = type_icon
     org[:text] ||= text
     org[:state] = { opened: true }
     org

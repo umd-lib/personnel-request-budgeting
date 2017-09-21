88f3cb05422f173d3a4e549c388a702b504ae624
David P. Steelman <dsteelma@umd.edu>
2017-09-21 11:50:36 -0400

    LIBITD-927. Restored "Report" model and associated classes
    Copied from v1.2.0, with minor tweaks to copy/paste issues with
    the comments, and test fixture usage ("test_admin" to "admin").
    
    https://issues.umd.edu/browse/LIBITD-927

---
+++ app/models/concerns/reportable.rb
@@ -0,0 +1,79 @@
+# Generic for all report type objects.
+#
+# Including this in your report adds common functionality and registers it in
+# the ReportManager
+module Reportable
+  extend ActiveSupport::Concern
+
+  class << self
+    def included(report)
+      report.extend ClassMethods
+      Report::Manager.register_report(report)
+    end
+  end
+
+  # These are class methods that are extended
+  module ClassMethods
+    # @return [Array<Symbol>] the parameters allowed for user input
+    def allowed_parameters
+      %i()
+    end
+
+    # @return [Array<String, Symbol>] the output formats this report is
+    #   available in.
+    def formats
+      %w(xlsx)
+    end
+
+    # @return [String] human-readable description of the report, displayed in
+    #   the GUI.
+    def description
+      'Text to be overridden in the Reportable subclass'
+    end
+
+    # @return [String] the view template to use in formatting the report output
+    def template
+      'shared/index'
+    end
+  end
+
+  attr_accessor :parameters
+
+  # @param parameters the parameters serialized in the master Report instance
+  def initialize(parameters = nil)
+    @parameters = parameters
+  end
+
+  # Override this method to define your query
+  # @return [Object] the data used by the template
+  def query; end
+
+  # alias to .formats
+  def formats
+    self.class.formats
+  end
+
+  # alias to .description
+  def description
+    self.class.description
+  end
+
+  # alias to .template
+  def template
+    self.class.template
+  end
+
+  # @return [Boolean] true if the provided parameters are valid, false
+  # otherwise. This default implementation always returns true.
+  def parameters_valid?
+    true
+  end
+
+  # @return [String,nil] a human-readable error message, or nil.
+  #   Typically used in conjunction with the "parameters_valid?" method to
+  #   describe why the parameters are invalid. This default implementation
+  #   returns nil.
+  def error_message
+    nil
+  end
+end
---
+++ app/models/report.rb
@@ -0,0 +1,68 @@
+# A basic generic report to be run
+class Report < ActiveRecord::Base
+  belongs_to :user
+  alias_attribute :creator, :user
+  serialize :parameters
+
+  # Provides a short human-readable description for this record, for GUI prompts
+  alias_attribute :description, :name
+
+  enum status: {
+    pending: 0,
+    running: 1,
+    error: 2,
+    completed: 3
+  }
+
+  enum format: {
+    xlsx: 0,
+    pdf: 1
+  }
+
+  %i(format status name).each { |f| validates f, presence: true }
+
+  # sugary method to get the report manager
+  def manager
+    Manager
+  end
+
+  def self.policy_class
+    ReportPolicy
+  end
+
+  # registers the available reports
+  class Manager
+    class << self
+      attr_accessor :reports, :allowed_parameters
+
+      # Add the report to our list of registered reports...
+      def register_report(klass)
+        @reports ||= []
+        @reports.unshift(klass) if klass.include?(Reportable) # last in, first up
+      end
+
+      def allowed_parameters
+        @reports.map(&:allowed_parameters).flatten
+      end
+
+      # really just a sanity check for Rails autoloading
+      def load_reports!
+        Dir[Rails.root.join('app', 'models', 'reports', '*_report.rb')].each { |f| require_dependency f }
+      end
+
+      def reports
+        load_reports! if @reports.blank?
+        @reports.map { |r| r.name.underscore }
+      end
+
+      # we go through our registered reports and see if we can get and
+      # instance.. to get and instance of TheCoolReport you pass the_cool_report
+      def report_for(report, *args)
+        load_reports! if @reports.blank?
+        @reports.each do |klass|
+          return klass.new(*args) if report == klass.name.underscore
+        end
+      end
+    end
+  end
+end

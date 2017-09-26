42b44ba62eb927e59db055e7cd8862093d22030a
David P. Steelman <dsteelma@umd.edu>
2017-09-26 13:17:55 -0400

    LIBITD-927. Implementation of "labor_requests_cost_summary_report"
    https://issues.umd.edu/browse/LIBITD-927

---
+++ app/models/reports/labor_requests_cost_summary_report.rb
@@ -0,0 +1,87 @@
+require 'reportable'
+# A summary report for the costs of Labor and Assistance requests
+class LaborRequestsCostSummaryReport
+  include Reportable
+
+  attr_reader :error_message
+
+  class << self
+    # @return [String] human-readable description of the report, displayed in
+    #   the GUI.
+    def description
+      'A summary report for the costs of Labor and Assistance requests, by division'
+    end
+
+    # @return [Array<String, Symbol>] the output formats this report is
+    #   available in.
+    def formats
+      %w(xlsx)
+    end
+
+    # @return [String] the view template to use in formatting the report output
+    def template
+      'shared/labor_requests_cost_summary'
+    end
+  end
+
+  def parameters_valid?
+    valid = parameters && parameters.key?(:review_status_ids)
+    return true if valid
+
+    @error_message = 'At least one review status must be specified!'
+    false
+  end
+
+  # @return [Object] the data used by the template
+  def query # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    # Stores annual cost totals, keyed by [department_code, emp_type_code]
+    annual_cost_totals = Hash.new(0)
+
+    # Stores nonop_fund totals, keyed by division_code
+    other_support_totals = Hash.new(0)
+
+    allowed_review_status_ids = parameters[:review_status_ids]
+    allowed_review_statuses = allowed_review_status_ids.map { |id| ReviewStatus.find(id) }
+
+    LaborRequest.includes(:organization, :review_status).each do |request|
+      review_status = request.review_status
+      next unless allowed_review_statuses.include?(review_status)
+      employee_type = request.employee_type
+      department_code = request.organization.code
+      key = [department_code, employee_type]
+      annual_cost = request.annual_cost
+      nonop_funds = request.nonop_funds
+      annual_cost_totals[key] += annual_cost unless annual_cost.nil?
+      other_support_key = [department_code, 'other_support']
+      other_support_totals[other_support_key] += nonop_funds unless nonop_funds.nil?
+    end
+
+    # Stores in an array (to preserve the department ordering), a "value" Hash
+    # representing that department's row in the table.
+    summary_data = []
+    Organization.department.order(:code).each do |dept|
+      department_code = dept.code
+      division_code = dept.parent.code
+      c1_key = [department_code, 'Contingent 1']
+      hourly_faculty_key = [department_code, 'Faculty Hourly']
+      students_key = [department_code, 'Student']
+      other_support_key = [department_code, 'other_support']
+      value = { department: dept.name,
+                division: division_code,
+                c1: annual_cost_totals[c1_key],
+                hourly_faculty: annual_cost_totals[hourly_faculty_key],
+                students: annual_cost_totals[students_key],
+                other_support: other_support_totals[other_support_key] }
+      summary_data << value
+    end
+
+    divisions = Organization.division
+    current_fiscal_year = I18n.t(:current_fiscal_year)
+    previous_fiscal_year = I18n.t(:previous_fiscal_year)
+
+    { summary_data: summary_data, divisions: divisions,
+      current_fiscal_year: current_fiscal_year,
+      previous_fiscal_year: previous_fiscal_year,
+      allowed_review_statuses: allowed_review_statuses }
+  end
+end

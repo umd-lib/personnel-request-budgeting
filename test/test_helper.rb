002c1596efbb009c9dcf6ad0c40463d3d911fe74
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2017-09-27 20:24:27 +0200

    LIBITD-988 Add chrome headless for jenkins
    This adds ability to run chrome headless for Jenkins. You can run it in
    regular mode by setting a ENV var of SELENIUM_CHROME.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -51,19 +51,38 @@ end
 # test/test_helper.rb:
 require 'capybara/rails'
 require 'capybara/minitest'
+require 'capybara-screenshot/minitest'
 
 Capybara.register_driver :chrome do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
 
+Capybara.register_driver :headless_chrome do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w[headless disable-gpu ] } 
+  ) 
+ 
+  Capybara::Selenium::Driver.new(app, browser: :chrome,
+                                desired_capabilities: caps )
+end
+
+# just the selenium screenshot driver.
+%i[ chrome headless_chrome ].each do |chrome_driver|
+  Capybara::Screenshot.register_driver(chrome_driver) do |driver, path|
+    driver.browser.save_screenshot(path) 
+  end
+end
+
+
+
 class ActionDispatch::IntegrationTest
  
   include Capybara::DSL
   include Capybara::Minitest::Assertions
- 
+  include Capybara::Screenshot::MiniTestPlugin 
 
   def use_chrome!
-    Capybara.current_driver = :chrome
+    Capybara.current_driver = ENV["SELENIUM_CHROME"] ? :chrome : :headless_chrome
     Capybara.page.driver.browser.manage.window.resize_to 1500, 800
   end
 

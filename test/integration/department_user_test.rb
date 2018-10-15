1804e5e9f8a7f5125cf0e069eab6d733f47c5730
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2018-10-15 10:42:25 +0200

    LIBITD-1133 Don't diplay cutoff warning if user still has edit ability
    A warning message was displayed for a record passing it's cutoff date
    regardless of if the user still had access. Only display this warning if
    the user is affected by cutoff
    
    https://issues.umd.edu/browse/LIBITD-1133

--- test/integration/department_user_test.rb
+++ test/integration/department_user_test.rb
@@ -28,4 +28,31 @@ class DepartmentUserTest < ActionDispatch::IntegrationTest
     assert page.has_content?('Labor and Assistance Requests successfully created.')
     refute page.has_content?('Unit is required for users with only Unit permissions')
   end
+
+  test 'should allow department user to make a unit request even if past unit deadline' do
+    cutoff = OrganizationCutoff.find(OrganizationCutoff.organization_types['unit'])
+    cutoff.cutoff_date = cutoff.cutoff_date - 1.year
+    cutoff.save
+    unit = @dept.organizations.find(&:unit?)
+
+    click_link 'Labor and Assistance'
+    click_link 'New'
+
+    select 'Faculty', from: 'Employee type'
+    select 'New', from: 'Request type'
+    fill_in 'Contractor name', with: SecureRandom.hex
+    fill_in 'Number of positions', with: 1
+    fill_in 'Hours per week', with: '9.99'
+    fill_in 'Hourly rate', with: '9.99'
+    fill_in 'Number of weeks', with: '9'
+    select @dept.name, from: 'Department'
+    select unit.name, from: 'Unit'
+    fill_in 'Justification', with: 'This is a test'
+    fill_in 'Position title', with: SecureRandom.hex
+    find('.page-header .btn-success').click
+
+    assert page.has_content?('Labor and Assistance Requests successfully created.')
+    refute page.has_content?('Unit is required for users with only Unit permissions')
+    refute page.has_content?('The submission window for this request has ended.')
+  end
 end

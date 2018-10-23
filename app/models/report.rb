49abecc823b4c002f981aa6281c51871bd9ed02e
David P. Steelman <dsteelma@umd.edu>
2018-10-23 15:19:13 -0400

    LIBITD-1275. Rubocop fix: Rails/ApplicationRecord
    https://issues.umd.edu/browse/LIBITD-1275

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # A basic generic report to be run
-class Report < ActiveRecord::Base
+class Report < ApplicationRecord
   belongs_to :user, optional: true
   alias_attribute :creator, :user
   serialize :parameters

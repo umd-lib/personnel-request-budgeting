36d01d6c82282b73c8bee95de8000162ed4b8ad5
David P. Steelman <dsteelma@umd.edu>
2018-10-24 16:16:30 -0400

    LIBITD-1286. Removed unnecessary configuration from test_helper.rb
    Removed old pre-Rails 5.1 configuration from test/test_helper.rb
    method.
    
    Moved some of the browser-focused methods from
    ActionDispatch::IntegrationTest into the ApplicationSystemTestCase
    class.
    
    https://issues.umd.edu/browse/LIBITD-1286

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,23 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  #  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  def javascript_errors
+    page.driver.browser.manage.logs.get(:browser)
+        .select { |e| e.level == 'SEVERE' && e.message.present? }
+        .collect(&:message)
+  end
+
+  def javascript_errors?
+    errors = javascript_errors # if you get the log, you clear the log...
+    raise JavaScriptError, errors.join("\n\n") if errors.present?
+  end
+
+  def teardown
+    javascript_errors?
+  end
 end
+
+class JavaScriptError < StandardError; end

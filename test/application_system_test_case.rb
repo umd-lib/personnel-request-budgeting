397bcbbb437ddd69bfd8ab98d9e65edccda7aa02
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2018-11-01 14:09:09 +0100

    LIBITD-1289 Add tests to export download
    This adds a test to the export download.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'action_pack'
+
+require 'fileutils'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   #  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  DOWNLOAD_DIR = File.join(Dir.tmpdir, 'downloads')
+
+  def self.profile
+    profile = Selenium::WebDriver::Chrome::Profile.new
+    profile['download.default_directory'] = DOWNLOAD_DIR
+    profile
+  end
+
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { profile: profile }
 
   def javascript_errors
     page.driver.browser.manage.logs.get(:browser)

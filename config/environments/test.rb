f1655f08725b2ceabc4a41e5e6ed03210efcbc0c
Chris Fitzpatrick <chrisfitzpat@gmail.com>
2018-11-01 11:03:44 +0100

    LIBITD-1294 Disable CAS SSL check on dev and test
    cas-rack is throwing an error when having to make a new ticket because
    the SSL cert check is failing. Disable ssl check on dev and test.
    
    https://issues.umd.edu/browse/LIBITD-1294

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.rack_cas.verify_ssl_cert = false
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
